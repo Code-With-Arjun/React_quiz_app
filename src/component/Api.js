0bdcdee40370b75b7b3348bc14ab3c0f2833661b
Code-With-Arjun <107105079+Code-With-Arjun@users.noreply.github.com>
2022-08-07 23:47:33 +0530

    Update Api.js

--- src/component/Api.js
+++ src/component/Api.js
@@ -4,7 +4,7 @@ import Home from './Home'
 
 const Api = (props) => {
     const [data, setData] = useState([])
-
+    const [isLoading,setIsLoading]=useState(true)
 
     useEffect(() => {
         axios.get(`https://opentdb.com/api.php?amount=${props.qno}&category=12&difficulty=${props.difficulty}&type=multiple`).then((e) => {
@@ -13,6 +13,7 @@ const Api = (props) => {
           console.log(props.difficulty)
           console.log(props.q)
           let x = e.data.results
+          setIsLoading(false);
           setData(x.map((e, index) => {
             return (
               {
@@ -21,11 +22,12 @@ const Api = (props) => {
                 question: e.question,
                 answers: [...e.incorrect_answers, e.correct_answer],
                 correct_answer: e.correct_answer
-              } ) }))  }) }, [])
-              console.log(data)
+              } ) }
+))  }) }, [])
+              
   return (
    <>
-   <Home data={data} name={props.name}/>
+   <Home data={data} name={props.name} isLoading={isLoading}/>
    </>
   )
 }
